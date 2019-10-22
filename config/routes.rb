8cd5d0454d719c549e27cc7cc6646ca8dfa51287
Santiago Llapur <santillapur@gmail.com>
2019-10-22 17:08:47 -0300

    Exercise 2 solved, mvc model with active record included in rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :tasks
+  # CREATE
+  # get 'tasks/new', to: 'tasks#new'
+  # post 'tasks', to: 'tasks#create'
+  # # READ
+  # get 'tasks', to: 'tasks#index'
+  # # /task/11 -> params[:id] == 11
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # # UPDATE
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  # patch 'tasks/:id', to: 'tasks#update'
+  # # DELETE
+  # delete 'tasks/:id', to: 'tasks#destroy'
 end
